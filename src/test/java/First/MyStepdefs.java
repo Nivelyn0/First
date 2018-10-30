bb8e1c75cb8d8c5eb51af6baa3715a8c3555accb
Nivelyn0 <puppy_shadow@ukr.net>
2018-10-31 00:27:35 +0200

    add remote driver

--- src/test/java/First/MyStepdefs.java
+++ src/test/java/First/MyStepdefs.java
@@ -3,16 +3,29 @@ package First;
 import cucumber.api.java.en.And;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MyStepdefs {
     WebDriver driver;
 
     @And("^Go to the google page$")
-    public void goToTheGooglePage() {
+    public void goToTheGooglePage() throws MalformedURLException {
+        String localSeleniumHubUrl = "http://127.0.0.1:4446/wd/hub";
+
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe"); // Windows path to webdriver executable
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
+        driver = new RemoteWebDriver(new URL(localSeleniumHubUrl),getChromeOptions());
         driver.manage().window().maximize();
         driver.get("http://demo.guru99.com/v4");
         driver.quit();
     }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        return options;}
 }
